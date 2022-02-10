d1862a118b6bd3308e4185514ddd0ca66b3a2fa5
SaifSushi <alushisaif@gmail.com>
2022-02-09 20:26:02 -0600

    login logic implemented all routes working updated alot of syntax

--- src/App.js
+++ src/App.js
@@ -1,20 +1,69 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
-import {Route, Link, Routes } from 'react-router-dom'
+import {Route, Link, Routes, useNavigate } from 'react-router-dom'
 import Login from './Authentication/Login';
 import SignUp from './Authentication/SignUp';
 import ResetPassword from './Authentication/ResetPassword';
 import Home from './Home';
 import Profile from './Profile';
+import axios from 'axios';
 
 function App() {
+const [userAuthToken, setUserAuthToken] = useState(null)
+const [userId, setUserId] = useState(null)  
+const [loggedIn, setLoggedIn] = useState(false)
+const navigate = useNavigate()
+const headers = {
+  'Content-Type': 'application/json',
+}
+
+  const loginRequest = (email, password) => {
+    const loginUrl = "http://localhost:8000/grouper/signin"
+    axios.post(loginUrl, {email: email, password: password}, { headers: headers})
+      .then((res) => {
+      setUserAuthToken(res.data)
+      console.log(res.data)
+      navigate("/home")
+    })
+    .catch(err => alert("oh shit"))
+  }
+
+  const storeId = (email) => {
+    const storeIdUrl = `http://localhost:8000/grouper/users/${email}` 
+    axios.get(storeIdUrl, {params: {
+      email: email
+    }}, { headers: headers} )
+    .then((res) => {
+      setUserId(res.data)
+      console.log(res)
+      console.log(userId)
+    })
+    .catch(err => alert("oh shit"))
+  }
+console.log(userAuthToken)
+console.log(loggedIn)
+
+useEffect(() => { 
+if(userAuthToken) {
+  setLoggedIn(true)
+}
+}, [userAuthToken])
+
+// if(userAuthToken){
+//   return(
+//     setLoggedIn(true))}
+console.log(`authToken: ${userAuthToken}`)
   return (
     <div>
   
 
     <main>
       <Routes>
-        <Route path="/login" element= {<Login/>}/>
+        <Route path="/login" element= {<Login 
+         loginRequest={ loginRequest } 
+         storeId={ storeId }
+         loggedIn = { loggedIn }
+        />}/>
         <Route path="/sign-up" element = {<SignUp/>}/>
         <Route path="/reset-password" element={ <ResetPassword/> }/>
         <Route path="/home" element={<Home/>}/>
